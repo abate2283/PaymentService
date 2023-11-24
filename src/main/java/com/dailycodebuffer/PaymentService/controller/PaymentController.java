09306a174d2bb87a7ee0eed2dc66a527bfd451a7
abate2283 <alfredbate@yahoo.co.uk>
2023-11-24 07:24:15 -0600

    Initial commit

---
+++ src/main/java/com/dailycodebuffer/PaymentService/controller/PaymentController.java
@@ -0,0 +1,11 @@
+package com.dailycodebuffer.PaymentService.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/payment")
+@Log4j2
+public class PaymentController {
+}
