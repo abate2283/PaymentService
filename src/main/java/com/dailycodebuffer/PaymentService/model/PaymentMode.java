02ed72cedd90394d233a26bb3a29d5aac5942bff
abate2283 <alfredbate@yahoo.co.uk>
2023-11-24 07:26:10 -0600

    PaymentMode added

---
+++ src/main/java/com/dailycodebuffer/PaymentService/model/PaymentMode.java
@@ -0,0 +1,13 @@
+package com.dailycodebuffer.PaymentService.model;
+
+public enum PaymentMode {
+    CREDIT_CARD,
+    DEBIT_CARD,
+    PAYPAL,
+    BITCOIN,
+    APPLE_PAY,
+    SAMSUNG_PAY,
+    GOOGLE_PAY,
+    BANK_TRANSFER,
+
+}
